59b3b42755b261c2f1245547a083d0972722723d
Derry_Jay <64017361+Derry-Jay@users.noreply.github.com>
2020-07-02 18:01:24 +0530

    serv

--- templates/signup.vue
+++ templates/signup.vue
@@ -1,7 +1,8 @@
-<html lang="en">
+<!--  --><template>
+<html>
   <head>
-  <script type = "text/javascript">
-  </script>
+    <script type = "text/javascript">
+    </script>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Signup</title>
@@ -30,102 +31,100 @@
     <link
       rel="apple-touch-icon"
       sizes="57x57"
-      href="img/apple-touch-icon-57x57.png"
+      href="{{ url_for('static',filename='img/apple-touch-icon-57x57.png') }}"
     />
     <link
       rel="apple-touch-icon"
       sizes="60x60"
-      href="img/apple-touch-icon-60x60.png"
+      href="{{ url_for('static',filename='img/apple-touch-icon-60x60.png') }}"
     />
     <link
       rel="apple-touch-icon"
       sizes="72x72"
-      href="img/apple-touch-icon-72x72.png"
+      href="{{ url_for('static',filename='img/apple-touch-icon-72x72.png') }}"
     />
     <link
       rel="apple-touch-icon"
       sizes="76x76"
-      href="img/apple-touch-icon-76x76.png"
+      href="{{ url_for('static',filename='img/apple-touch-icon-76x76.png') }}"
     />
     <link
       rel="apple-touch-icon"
       sizes="114x114"
-      href="img/apple-touch-icon-114x114.png"
+      href="{{ url_for('static',filename='img/apple-touch-icon-114x114.png') }}"
     />
     <link
       rel="apple-touch-icon"
       sizes="120x120"
-      href="img/apple-touch-icon-120x120.png"
+      href="{{ url_for('static',filename='img/apple-touch-icon-120x120.png') }}"
     />
     <link
       rel="apple-touch-icon"
       sizes="144x144"
-      href="img/apple-touch-icon-144x144.png"
+      href="{{ url_for('static',filename='img/apple-touch-icon-144x144.png') }}"
     />
     <link
       rel="apple-touch-icon"
       sizes="152x152"
-      href="img/apple-touch-icon-152x152.png"
+      href="{{ url_for('static',filename='img/apple-touch-icon-152x152.png') }}"
     />
     <link
       rel="apple-touch-icon"
       sizes="180x180"
-      href="img/apple-touch-icon-180x180.png"
+      href="{{ url_for('static',filename='img/apple-touch-icon-180x180.png') }}"
     />
     <link
       rel="icon"
       type="image/png"
-      href="img/favicon-32x32.png"
+      href="{{ url_for('static',filename='img/favicon-32x32.png') }}"
       sizes="32x32"
     />
     <link
       rel="icon"
       type="image/png"
-      href="img/android-chrome-192x192.png"
+      href="{{ url_for('static',filename='img/android-chrome-192x192.png') }}"
       sizes="192x192"
     />
     <link
       rel="icon"
       type="image/png"
-      href="img/favicon-96x96.png"
+      href="{{ url_for('static',filename='img/favicon-96x96.png') }}"
       sizes="96x96"
     />
     <link
       rel="icon"
       type="image/png"
-      href="img/favicon-16x16.png"
+      href="{{ url_for('static',filename='img/favicon-16x16.png') }}"
       sizes="16x16"
     />
-    <link rel="manifest" href="img/manifest.json" />
+    <link rel="manifest" href="{{ url_for('static',filename='img/manifest.json') }}" />
     <meta name="msapplication-TileColor" content="#da532c" />
-    <meta name="msapplication-TileImage" content="img/mstile-144x144.png" />
+    <meta name="msapplication-TileImage" content="{{ url_for('static',filename='img/mstile-144x144.png') }}" />
     <meta name="theme-color" content="#ffffff" />
-    <link rel="stylesheet" href="css/animsition.min.css" />
-    <link rel="stylesheet" type="text/css" href="css/grid.min.css" />
-    <link rel="stylesheet" type="text/css" href="css/style.css" />
-    <link rel="stylesheet" type="text/css" href="css/menu.css" />
-    <link rel="stylesheet" type="text/css" href="css/social.css" />
-    <link rel="stylesheet" type="text/css" href="css/slickform.css" />
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.1/animate.min.css"
-    />
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css"
-    />
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome-animation/0.0.8/font-awesome-animation.min.css"
-    />
+    <link rel="stylesheet" type="text/css" href="{{ url_for('static',filename='css/grid.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ url_for('static',filename='css/style.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ url_for('static',filename='css/menu.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ url_for('static',filename='css/social.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ url_for('static',filename='css/animsition.min.css') }}">
+    <!-- 
+    <link>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.1/animate.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome-animation/0.0.8/font-awesome-animation.min.css"/> -->
+    <!-- 
+    <style src="{{ url_for('static',filename='css/slickform.css') }}"></style> -->
+    <style src="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.1/animate.min.css"></style>
+    <style src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css"></style>
+    <style src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome-animation/0.0.8/font-awesome-animation.min.css"></style>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <script type="text/javascript" src="js/jquery.matchHeight-min.js"></script>
-    <script src="js/wow.min.js"></script>
-    <script src="js/animsition.min.js"></script>
-    <script src="js/Slickform.js"></script>
-    <script src="js/functions.js"></script>
-    <script src="val.js"></script>
+    <script src="{{ url_for('static',filename='js/jquery.matchHeight-min.js') }}"></script>
+    <script src="{{ url_for('static',filename='js/wow.min.js') }}"></script>
+    <script src="{{ url_for('static',filename='js/animsition.min.js') }}"></script>
+    <script src="{{ url_for('static',filename='js/Slickform.js') }}"></script>
+    <script src="{{ url_for('static',filename='js/functions.js') }}"></script>
+    <script src="{{ url_for('static',filename='js/val.js') }}"></script>
+    <script src="{{ url_for('static',filename='js/vue.js') }}"></script>
   </head>
   <body>
     <div class="animsition-overlay">
@@ -180,7 +179,6 @@
                   type="text"
                   value=""
                   onblur="isname(this.value)"
-                  id="username"
                   required=""
                 />
                 <br />
@@ -201,7 +199,6 @@
                   type="email"
                   value=""
                   onblur="ismail(this.value)"
-                  id="mailid"
                   required=""
                 />
                 <br />
@@ -222,7 +219,6 @@
                   type="text"
                   value=""
                   onblur="iscity(this.value)"
-                  id="textfield5"
                   required=""
                 />
                 <br />
@@ -234,7 +230,6 @@
                   value=""
                   maxlength="10"
                   onblur="ismobile(this.value)"
-                  id="mobile"
                   required=""
                 />
                 <br />
@@ -245,7 +240,6 @@
                   type="password"
                   value=""
                   onblur="ispass(this.value)"
-                  id="password1"
                   required=""
                 />
                 <br />
@@ -594,3 +588,4 @@ function myFunction() {
 </script>
   </body>
 </html>
+<!--  --></template>
