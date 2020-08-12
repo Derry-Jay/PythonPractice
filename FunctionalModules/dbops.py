b62acb89d525505cf371f1653457cc635cc347d1
Derry_Jay <64017361+Derry-Jay@users.noreply.github.com>
2020-08-12 12:42:37 +0530

    temp

---
+++ FunctionalModules/dbops.py
@@ -0,0 +1,30 @@
+import psycopg2 as pypg
+cqs = "select count(*) from public.registered_users where "
+snt = "select user_name,user_type"
+con = pypg.connect(user='postgres', password='root', database='postgres')
+cur = con.cursor()
+def ins(p):
+    if p != []:
+        cur.execute(cqs+"user_name='%s' and user_type='%s' and mail_id='%s' and password='%s'" %
+                    (p[0], p[1], p[2], p[4]))
+        pct = cur.fetchone()
+        pc = 0
+        for i in pct:
+            pc = i
+        qs = "insert into public.registered_users(user_name, user_type, user_mail, date_of_birth, gender, city, phone_no, password) values ('" + \
+            p[0]+"','" + p[1] + "','" + p[2]+"','"+p[3]+"','"+p[4]+"')"
+        if pc == 0:
+            cur.execute(qs)
+            con.commit()
+            p = []
+
+
+def get(p):
+    cur.execute(cqs+"mail_id='%s' and password='%s'" % (p[0], p[1]))
+    prc = cur.fetchone()
+    return prc[0]
+
+
+def get1(q):
+    cur.execute(snt+" from registered_users where mail_id='%s' and password='%s'" % (q[0], q[1]))
+    return cur.fetchone()
---
+++ static/js/import.js
@@ -0,0 +1,7 @@
+const linker = new VueLink({
+    link: [
+        { rel: "apple-touch-icon", sizes: "57x57" },
+        { rel: "apple-touch-icon", sizes: "60x60" },
+    ],
+});
+const meta = new VueMeta();
